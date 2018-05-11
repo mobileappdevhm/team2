73a93f380a97d23de57b3f4eb9b0c4e1a9a9a8e1
hmkrivoj <krivoj@hm.edu>
2018-05-11 20:58:32 +0200

    Homepage with BottomNavigationBar (bnbHome)

---
+++ lib/ui/scaffolds/bnbHome.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:courses_in_english/ui/screens/sampleScreen.dart';
+
+class HomeScaffold extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _HomeScaffoldState();
+}
+
+class _HomeScaffoldState extends State<HomeScaffold> {
+  static final int _initialIndex = 2;
+
+  int _selectedIndex = _initialIndex;
+  PageController _controller = new PageController(initialPage: _initialIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      bottomNavigationBar: new BottomNavigationBar(
+        items: [
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.import_contacts),
+              title: new Text('Courses')),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.map), title: new Text('Maps')),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.calendar_today),
+              title: new Text('Timetable')),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.favorite_border),
+              title: new Text('Favorites')),
+          new BottomNavigationBarItem(
+              icon: new Icon(Icons.settings), title: new Text('Settings')),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (newIndex) {
+          setState(() {
+            _selectedIndex = newIndex;
+            _controller.jumpToPage(newIndex);
+          });
+        },
+      ),
+      appBar: new AppBar(
+        title: new Text('Courses in English'),
+      ),
+      body: new PageView(
+        children: <Widget>[
+          new SampleScreen('Courses'),
+          new SampleScreen('Maps'),
+          new SampleScreen('Timetable'),
+          new SampleScreen('Favorites'),
+          new SampleScreen('Settings'),
+        ],
+        onPageChanged: (newIndex) {
+          setState(() {
+            _selectedIndex = newIndex;
+          });
+        },
+        controller: _controller,
+      ),
+    );
+  }
+}
