5cb33f787c84bfe7272e3c6927606bfe664711ea
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2018-05-25 11:09:57 +0200

    minor

--- test/course_list_test.dart
+++ test/course_list_test.dart
@@ -7,29 +7,27 @@ import 'package:courses_in_english/ui/screens/course_list_screen.dart';
 import 'package:courses_in_english/connect/dataprovider/course/mock/mock_course_provider.dart';
 import 'package:courses_in_english/model/course/course.dart';
 
-Future pause(Duration d) => new Future.delayed(d);
+// Future pause(Duration d) => new Future.delayed(d);
 
 void main() {
-
   final MockCourseProvider courseProvider = new MockCourseProvider();
   final Future<List<Course>> courses_future = courseProvider.getCourses();
   List<Course> courses;
 
-    testWidgets('Test Course List Item Informations',
+  testWidgets('Test Course List Item Informations',
       (WidgetTester tester) async {
-    await tester.pumpWidget(
-      new MaterialApp(
-        home: new CourseListScreen(),
-      ),
-    );
-
     await courses_future.then((value) {
-    courses = value;
+      courses = value;
     });
 
+    await tester.pumpWidget(
+        new MaterialApp(
+          home: new CourseListScreen(courses),
+        ),
+        new Duration(seconds: 1));
 
-    await pause(const Duration(milliseconds: 800));
-
+    print("Yay");
+    // await pause(const Duration(milliseconds: 800));
 
     for (var course in courses) {
       expect(find.text(course.name), findsOneWidget);
@@ -42,11 +40,12 @@ void main() {
       (WidgetTester tester) async {
     await tester.pumpWidget(
       new MaterialApp(
-        home: new CourseListScreen(),
+        home: new CourseListScreen(courses),
       ),
     );
 
-    await pause(const Duration(milliseconds: 800));
+    print("YAY2");
+    // await pause(const Duration(milliseconds: 800));
 
     expect(find.byIcon(Icons.favorite_border), findsNWidgets(courses.length));
     expect(find.byIcon(Icons.favorite), findsNothing);
@@ -61,4 +60,3 @@ void main() {
     expect(find.byIcon(Icons.favorite), findsNothing);
   });
 }
-
