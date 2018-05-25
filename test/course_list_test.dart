5523d1751de457ffc0e9fb3f01c009300edb3411
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2018-05-25 08:39:23 +0200

    Adds Test prototype for course list

---
+++ test/course_list_test.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:courses_in_english/ui/screens/course_list_screen.dart';
+import 'package:courses_in_english/connect/dataprovider/course/mock/mock_course_provider.dart';
+import 'package:courses_in_english/model/course/course.dart';
+
+Future pause(Duration d) => new Future.delayed(d);
+
+void main() {
+
+  final MockCourseProvider courseProvider = new MockCourseProvider();
+  final Future<List<Course>> courses_future = courseProvider.getCourses();
+  List<Course> courses;
+
+    testWidgets('Test Course List Item Informations',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      new MaterialApp(
+        home: new CourseListScreen(),
+      ),
+    );
+
+    await courses_future.then((value) {
+    courses = value;
+    });
+
+
+    await pause(const Duration(milliseconds: 800));
+
+
+    for (var course in courses) {
+      expect(find.text(course.name), findsOneWidget);
+      expect(find.text(course.lecturerName), findsOneWidget);
+      expect(find.text("FK " + course.department.toString()), findsOneWidget);
+    }
+  });
+
+  testWidgets('Test Course List Item favorite mechanism',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      new MaterialApp(
+        home: new CourseListScreen(),
+      ),
+    );
+
+    await pause(const Duration(milliseconds: 800));
+
+    expect(find.byIcon(Icons.favorite_border), findsNWidgets(courses.length));
+    expect(find.byIcon(Icons.favorite), findsNothing);
+    await tester.tap(find.byIcon(Icons.favorite_border).first);
+    await tester.pump();
+    expect(find.byIcon(Icons.favorite), findsOneWidget);
+    expect(
+        find.byIcon(Icons.favorite_border), findsNWidgets(courses.length - 1));
+    await tester.tap(find.byIcon(Icons.favorite));
+    await tester.pump();
+    expect(find.byIcon(Icons.favorite_border), findsNWidgets(courses.length));
+    expect(find.byIcon(Icons.favorite), findsNothing);
+  });
+}
+
