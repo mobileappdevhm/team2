e4007939278d992f1ecd881f05c86f01b3133b3e
Gleydar <gleydar@outlook.de>
2018-05-28 12:25:53 +0200

    Tried to nicely format Settings

--- lib/ui/scaffolds/bnb_home.dart
+++ lib/ui/scaffolds/bnb_home.dart
@@ -5,6 +5,7 @@ import 'package:courses_in_english/ui/screens/favorites_screen.dart';
 import 'package:courses_in_english/ui/screens/locations_screen.dart';
 import 'package:courses_in_english/ui/screens/course_list_screen.dart';
 import 'package:courses_in_english/ui/screens/sample_screen.dart';
+import 'package:courses_in_english/ui/screens/settings_screen.dart';
 import 'package:courses_in_english/ui/screens/timetable_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -50,7 +51,7 @@ class _HomeScaffoldState extends State<HomeScaffold> {
         new LocationScreen(campuses),
         new TimetableScreen(courses),
         new FavoriteListScreen(courses),
-        new SampleScreen('Settings'),
+        new SettingsScreen(),
       ];
       scaffold = new Scaffold(
         bottomNavigationBar: new BottomNavigationBar(
--- lib/ui/screens/settings_screen.dart
+++ lib/ui/screens/settings_screen.dart
@@ -11,23 +11,72 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     return new Column(
       children: <Widget>[
-        new Container(
-          child: new Row(
-            children: <Widget>[
-              new Text('Hi'),
-              new Switch(value: true, onChanged: (bool status) {})
-            ],
-            crossAxisAlignment: CrossAxisAlignment.center,
-          ),
-          padding: new EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
-        ),
-        new Row(
-          children: <Widget>[
-            new LineSeparator(title: 'Other Settings')
-          ],
-        )
+        _newElement([
+          new LineSeparator(
+            title: 'General Settings',
+            isBold: true,
+          )
+        ]),
+        _newElement([
+          new LineSeparator(
+            title: 'Synchronisation Settings',
+          )
+        ]),
+        _newElement([
+          Text('Enable automatic sync'),
+          Switch(value: false, onChanged: (bool changed) {}),
+        ]),
+        _newElement([
+          new Text('Offline mode'),
+          new Switch(value: false, onChanged: (bool changed) {})
+        ]),
+        _newElement([
+          new LineSeparator(
+            title: 'Secret Settings ;)',
+          )
+        ]),
+        _newElement([
+          new Text('Ficke deine Mutter'),
+          new Switch(value: true, onChanged: (bool changed) {})
+        ]),
+        _newElement([
+          new Text('Enable Secret 1'),
+          new Switch(value: false, onChanged: (bool changed) {})
+        ]),
+        _newElement([
+          new LineSeparator(
+            title: 'UI Settings',
+            isBold: true,
+          )
+        ]),
+        _newElement([
+          new Text('Use alternative layout'),
+          new Switch(value: false, onChanged: (bool changed) {})
+        ]),
+        _newElement([
+          new LineSeparator(
+            title: 'Language Settings',
+            isBold: true,
+          )
+        ]),
       ],
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     );
+  }
+
+  Widget _newElement(var children) {
+    if (children[0] is LineSeparator) {
+      return new Row(
+        children: children,
+      );
+    } else {
+      return Container(
+        child: Row(
+          children: children,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+        padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 30.0),
+      );
+    }
   }
 }
