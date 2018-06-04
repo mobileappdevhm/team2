638ea7bbbfa090ea4b66a762f4d89aedf830996c
l0renor <leon.lukas11@web.de>
2018-06-04 23:52:22 +0200

    reformat

--- lib/ui/basic_components/course_list_entry.dart
+++ lib/ui/basic_components/course_list_entry.dart
@@ -1,11 +1,11 @@
+import 'package:courses_in_english/connect/dataprovider/data.dart';
 import 'package:courses_in_english/connect/dataprovider/favorites/favorites_observer.dart';
-import 'package:flutter/material.dart';
 import 'package:courses_in_english/model/course/course.dart';
 import 'package:courses_in_english/model/department/department.dart';
-import 'package:courses_in_english/connect/dataprovider/data.dart';
-import '../scaffolds/course_details.dart';
-import 'package:courses_in_english/model/course/time_and_day.dart';
 import 'package:courses_in_english/ui/basic_components/availability_widget.dart';
+import 'package:flutter/material.dart';
+
+import '../scaffolds/course_details.dart';
 
 class CourseListEntry extends StatefulWidget {
   final Course course;
