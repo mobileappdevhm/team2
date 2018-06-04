edeb1c50379da0df674958291be49bf1441276b5
Gleydar <gleydar@outlook.de>
2018-06-04 23:07:21 +0200

    Fixed some settings

--- lib/connect/dataprovider/user/mock/mock_user_settings_provider.dart
+++ lib/connect/dataprovider/user/mock/mock_user_settings_provider.dart
@@ -2,8 +2,10 @@ import 'package:courses_in_english/connect/dataprovider/user/user_settings_provi
 import 'package:courses_in_english/model/user/user_settings.dart';
 
 class MockUserSettingsProvider extends UserSettingsProvider {
+  UserSettings _settings = new UserSettings();
+
   @override
   UserSettings getCurrentSettings() {
-    return new UserSettings();
+    return _settings;
   }
 }
---
+++ test/settings_screen_test.dart
@@ -0,0 +1,16 @@
+import 'package:courses_in_english/connect/dataprovider/data.dart';
+import 'package:courses_in_english/connect/dataprovider/user/user_settings_provider.dart';
+import 'package:courses_in_english/ui/screens/settings_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('SettingsScreenTest', (WidgetTester tester) async {
+    UserSettingsProvider _data = new Data().settingsProvider;
+    await tester.pumpWidget(
+        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
+    expect(_data.getCurrentSettings().autoSync, false);
+    await tester.tap(find.byType(Switch).first);
+    expect(_data.getCurrentSettings().autoSync, true);
+  });
+}
