1d892f39415d0b617d13f8758c2675f19942958c
Gleydar <gleydar@outlook.de>
2018-06-06 17:05:02 +0200

    Added firebase, reformatted pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:courses_in_english/ui/scaffolds/login.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new CieApp());
 
-class MyApp extends StatelessWidget {
+final FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
+
+class CieApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {});
+    _firebaseMessaging.getToken().then((String token) {});
+
     return new MaterialApp(
       title: 'Cie App',
       theme: new ThemeData(
