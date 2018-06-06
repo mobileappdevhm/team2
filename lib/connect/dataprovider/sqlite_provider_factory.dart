47c5fed6a462f2e753ae9fc2f83cc2818a11aca7
Gleydar <gleydar@outlook.de>
2018-06-06 16:23:03 +0200

    Fixed factory

--- lib/connect/dataprovider/sqlite_provider_factory.dart
+++ lib/connect/dataprovider/sqlite_provider_factory.dart
@@ -1,8 +1,8 @@
 import 'package:courses_in_english/connect/dataprovider/campus/campus_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/campus/mock/sqlite_campus_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/course/course_provider.dart';
-import 'package:courses_in_english/connect/dataprovider/course/mock/sqlite_course_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/course/mock/mock_selection_provider.dart';
+import 'package:courses_in_english/connect/dataprovider/course/mock/sqlite_course_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/course/selection_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/department/department_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/department/mock/sqlite_department_provider.dart';
@@ -12,7 +12,9 @@ import 'package:courses_in_english/connect/dataprovider/lecturer/lecturer_provid
 import 'package:courses_in_english/connect/dataprovider/lecturer/mock/sqlite_lecturer_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/provider_factory.dart';
 import 'package:courses_in_english/connect/dataprovider/user/mock/mock_user_provider.dart';
+import 'package:courses_in_english/connect/dataprovider/user/mock/mock_user_settings_provider.dart';
 import 'package:courses_in_english/connect/dataprovider/user/user_provider.dart';
+import 'package:courses_in_english/connect/dataprovider/user/user_settings_provider.dart';
 
 /// Provider factory producing mock providers.
 class SqliteProviderFactory implements ProviderFactory {
@@ -37,4 +39,8 @@ class SqliteProviderFactory implements ProviderFactory {
 
   @override
   CampusProvider createCampusProvider() => new SqliteCampusProvider();
+
+  @override
+  UserSettingsProvider createSettingsProvider() =>
+      new MockUserSettingsProvider();
 }
