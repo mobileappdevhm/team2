c3e74ebd232c4ceac1515a51954a67836095ad33
Mario Meissner <mr.rash.boy@gmail.com>
2018-06-07 20:09:41 +0200

    Rewrite course_list_screen as stateless

--- lib/ui/screens/course_list_screen.dart
+++ lib/ui/screens/course_list_screen.dart
@@ -3,45 +3,25 @@ import 'package:courses_in_english/model/department/department.dart';
 import 'package:courses_in_english/model/course/course.dart';
 import 'package:courses_in_english/ui/basic_components/course_list_entry.dart';
 
-class CourseListScreen extends StatefulWidget {
+class CourseListScreen extends StatelessWidget {
   final List<Course> courseList;
   final Iterable<Department> departments;
-
+  //final CourseListEntry courseWidgets;
   CourseListScreen(this.courseList, this.departments);
 
   @override
-  CourseListState createState() =>
-      new CourseListState(this.courseList, this.departments);
-}
-
-class CourseListState extends State<CourseListScreen> {
-  final List<Course> courseList;
-  final Iterable<Department> departments;
-
-  CourseListState(this.courseList, this.departments);
-
-  @override
   Widget build(BuildContext context) {
-    Widget body;
-    if (courseList.isEmpty) {
-      body = loadingScreenView();
-    } else {
-      body = courseListView();
-    }
-
-    return new Center(
-      child: body,
-    );
-  }
-
-  Widget loadingScreenView() {
-    return new Container(
+    return Center(
+        child: Container(
+      constraints: BoxConstraints.expand(),
       alignment: Alignment.center,
-      child: new Text('Animation Time', textAlign: TextAlign.center),
-    );
+      child: ListView(
+        children: createCourseWidgets(),
+      ),
+    ));
   }
 
-  List<Widget> courseWidgets() {
+  List<Widget> createCourseWidgets() {
     List<Widget> courseWidgets = new List<Widget>();
     for (Course course in courseList) {
       Department department = departments
@@ -49,15 +29,5 @@ class CourseListState extends State<CourseListScreen> {
       courseWidgets.add(new CourseListEntry(course, department));
     }
     return courseWidgets;
-  }
-
-  Widget courseListView() {
-    return new Container(
-      constraints: new BoxConstraints.expand(),
-      alignment: Alignment.center,
-      child: new ListView(
-        children: courseWidgets(),
-      ),
-    );
   }
 }
