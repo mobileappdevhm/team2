981dda88334f222e5422bac5eb5122268d16002a
MartinGaiser <martin-gaiser@t-online.de>
2018-06-10 15:32:22 +0200

    added Layout test

---
+++ test/login_test.dart
@@ -0,0 +1,34 @@
+import 'package:courses_in_english/ui/basic_components/line_separator.dart';
+import 'package:courses_in_english/ui/basic_components/scenery_widget.dart';
+import 'package:courses_in_english/ui/scaffolds/login.dart';
+import 'package:flutter/material.dart';
+import "package:flutter_test/flutter_test.dart";
+
+
+
+void main(){
+  testWidgets("testLoginLayout", (WidgetTester tester) async{
+    await tester.pumpWidget(new MaterialApp(home: new LoginScreen()));
+    expect(find.byType(RaisedButton), findsNWidgets(2));
+    expect(find.byType(TextFormField), findsNWidgets(2));
+    expect(find.byType(SceneryWidget), findsOneWidget);
+    expect(find.byType(LineSeparator), findsOneWidget);
+  });
+
+  testWidgets("testLogin", (WidgetTester tester) async{
+  });
+
+  testWidgets("testLoginNoAT", (WidgetTester tester) async{
+
+  });
+
+  testWidgets("testLoginNoDot", (WidgetTester tester) async{
+
+  });
+  testWidgets("testLogin", (WidgetTester tester) async{
+
+  });
+  testWidgets("testLogin", (WidgetTester tester) async{
+
+  });
+}
