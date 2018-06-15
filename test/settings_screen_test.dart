98afdee26599926daa7ee622501b4feabc368265
Gleydar <gleydar@outlook.de>
2018-06-15 16:04:02 +0200

    Had to remove some tests, sadly

--- test/settings_screen_test.dart
+++ test/settings_screen_test.dart
@@ -1,74 +1,74 @@
-//import 'package:courses_in_english/connect/dataprovider/data.dart';
-//import 'package:courses_in_english/connect/dataprovider/user/user_settings_provider.dart';
-//import 'package:courses_in_english/controller/session.dart';
-//import 'package:courses_in_english/model/user/user_settings.dart';
-//import 'package:courses_in_english/ui/screens/settings_screen.dart';
-//import 'package:flutter/material.dart';
-//import 'package:flutter_test/flutter_test.dart';
+import 'package:courses_in_english/connect/dataprovider/data.dart';
+import 'package:courses_in_english/connect/dataprovider/user/user_settings_provider.dart';
+import 'package:courses_in_english/controller/session.dart';
+import 'package:courses_in_english/model/user/user_settings.dart';
+import 'package:courses_in_english/ui/screens/settings_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+//  testWidgets('notLoggedInTest', (WidgetTester tester) async {
+//    Session _data = new Session();
+//    _data.setSettings(new UserSettings());
+//    await tester.pumpWidget(
+//        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
+//    expect(_data.settings.offlineMode, true);
+//    await tester.tap(find.byType(Switch).first);
+//    expect(_data.settings.autoSync, false);
+//  });
 //
-//void main() {
-////  testWidgets('notLoggedInTest', (WidgetTester tester) async {
-////    Session _data = new Session();
-////    _data.setSettings(new UserSettings());
-////    await tester.pumpWidget(
-////        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
-////    expect(_data.settings.offlineMode, true);
-////    await tester.tap(find.byType(Switch).first);
-////    expect(_data.settings.autoSync, false);
-////  });
-////
-////  testWidgets('OfflineModeTest', (WidgetTester tester) async {
-////    Session _data = new Session();
-////    _data.setSettings(new UserSettings());
-////    await tester.pumpWidget(
-////        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
-////    expect(_data.settings.feedbackMode, true);
-////    await tester.tap(find.byType(Switch).at(1));
-////    expect(_data.settings.feedbackMode, false);
-////  });
+//  testWidgets('OfflineModeTest', (WidgetTester tester) async {
+//    Session _data = new Session();
+//    _data.setSettings(new UserSettings());
+//    await tester.pumpWidget(
+//        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
+//    expect(_data.settings.feedbackMode, true);
+//    await tester.tap(find.byType(Switch).at(1));
+//    expect(_data.settings.feedbackMode, false);
+//  });
+
+//  testWidgets('AutoSyncTest', (WidgetTester tester) async {
 //
-////  testWidgets('AutoSyncTest', (WidgetTester tester) async {
-////
-////    await tester.pumpWidget(
-////        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
-////    UserSettingsProvider _data = new Data().settingsProvider;
-////    UserSettings us = await _data.getCurrentSettings();
-////    expect(us.offlineMode, true);
-////    expect(us.feedbackMode, false);
-////    expect(find.text("General Settings"), findsOneWidget);
-////
-////    expect(find.text("Guest Users can't save settings"), findsOneWidget);
-////    expect(find.byType(LineSeparator), findsOneWidget);
-////  });
-////
-////  testWidgets('OfflineModeTest', (WidgetTester tester) async {
-////    UserSettingsProvider _data = new Data().settingsProvider;
-////    await tester.pumpWidget(
-////        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
-////    expect(_data.getCurrentSettings().offlineMode, false);
-////    await tester.tap(find.byType(Switch).at(1));
-////    expect(_data.getCurrentSettings().offlineMode, true);
-////  });
+//    await tester.pumpWidget(
+//        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
+//    UserSettingsProvider _data = new Data().settingsProvider;
+//    UserSettings us = await _data.getCurrentSettings();
+//    expect(us.offlineMode, true);
+//    expect(us.feedbackMode, false);
+//    expect(find.text("General Settings"), findsOneWidget);
 //
-////  testWidgets('feedbackModeModeTest', (WidgetTester tester) async {
-////    UserSettingsProvider _data = new Data().settingsProvider;
-////    await tester.pumpWidget(
-////        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
-////    expect(_data.getCurrentSettings().feedbackMode, false);
-////    await tester.tap(find.byType(Switch).at(2));
-////    expect(_data.getCurrentSettings().feedbackMode, true);
-////  });
+//    expect(find.text("Guest Users can't save settings"), findsOneWidget);
+//    expect(find.byType(LineSeparator), findsOneWidget);
+//  });
 //
-////
-////  testWidgets('otherSettingsTest', (WidgetTester tester) async {
-////    UserSettingsProvider _data = new Data().settingsProvider;
-////    await tester.pumpWidget(
-////        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
-////    expect(find.widgetWithText(TextFormField, "Sync Favorites"),
-////      findsOneWidget);
-////    expect(find.widgetWithText(TextFormField, "Push Favorites"),
-////      findsOneWidget);
-////    expect(find.widgetWithText(TextFormField, "Sync Options"),
-////      findsOneWidget);
-////  });
-//}
+//  testWidgets('OfflineModeTest', (WidgetTester tester) async {
+//    UserSettingsProvider _data = new Data().settingsProvider;
+//    await tester.pumpWidget(
+//        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
+//    expect(_data.getCurrentSettings().offlineMode, false);
+//    await tester.tap(find.byType(Switch).at(1));
+//    expect(_data.getCurrentSettings().offlineMode, true);
+//  });
+
+//  testWidgets('feedbackModeModeTest', (WidgetTester tester) async {
+//    UserSettingsProvider _data = new Data().settingsProvider;
+//    await tester.pumpWidget(
+//        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
+//    expect(_data.getCurrentSettings().feedbackMode, false);
+//    await tester.tap(find.byType(Switch).at(2));
+//    expect(_data.getCurrentSettings().feedbackMode, true);
+//  });
+
+//
+//  testWidgets('otherSettingsTest', (WidgetTester tester) async {
+//    UserSettingsProvider _data = new Data().settingsProvider;
+//    await tester.pumpWidget(
+//        new MaterialApp(home: new Scaffold(body: new SettingsScreen())));
+//    expect(find.widgetWithText(TextFormField, "Sync Favorites"),
+//      findsOneWidget);
+//    expect(find.widgetWithText(TextFormField, "Push Favorites"),
+//      findsOneWidget);
+//    expect(find.widgetWithText(TextFormField, "Sync Options"),
+//      findsOneWidget);
+//  });
+}
