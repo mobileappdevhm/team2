39e47f9d48392c23c9c92809170803d76c445e89
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2018-06-22 01:54:19 +0200

    Adds network campus provider

---
+++ lib/io/inet/providers/prod/campus_provider.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:courses_in_english/io/inet/http/http_helper.dart';
+import 'package:courses_in_english/io/inet/providers/campus_provider.dart';
+import 'package:courses_in_english/io/mock_data.dart';
+import 'package:courses_in_english/model/campus/campus.dart';
+
+/// Provider for campuses providing mock data.
+
+//TODO Change Campus object design to fit server response
+
+class ProdCampusProvider implements InetCampusProvider {
+  final HttpHelper helper;
+
+  ProdCampusProvider(this.helper);
+
+  @override
+  Future<List<Campus>> getCampuses() async => helper
+      .getCampuses()
+      .then((list) => list.map((campus) => parseCampus(campus)).toList());
+
+  Campus parseCampus(Map<String, dynamic> json) {
+    return new Campus(json['id'], json['name'], 'image', 'address');
+  }
+}
