0a8dbc18aef350ef0a25780e83ecd164a110a8c3
MartinGaiser <martin-gaiser@t-online.de>
2018-06-22 15:57:27 +0200

    removed unused imports in add_cie_test

--- test/add_cie_test.dart
+++ test/add_cie_test.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:courses_in_english/ui/basic_components/line_separator.dart';
 
 void main() {
 //  testWidgets('notLoggedInTest', (WidgetTester tester) async {
