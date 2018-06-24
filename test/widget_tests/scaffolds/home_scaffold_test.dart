f2076340b96bb3debb564912d7c69f44d46bf337
Hayden <hudgy.1@gmail.com>
2018-06-24 18:45:34 +0200

    fixed testing bugs

--- test/widget_tests/scaffolds/home_scaffold_test.dart
+++ test/widget_tests/scaffolds/home_scaffold_test.dart
@@ -3,6 +3,8 @@ import 'package:courses_in_english/io/cache/mocked_providers_factory.dart';
 import 'package:courses_in_english/io/inet/mockito_inet_provider_factory.dart';
 import 'package:courses_in_english/model/content.dart';
 import 'package:courses_in_english/model/course/course.dart';
+import 'package:courses_in_english/model/department/department.dart';
+import 'package:courses_in_english/model/user/user.dart';
 import 'package:courses_in_english/ui/scaffolds/bnb_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -18,7 +20,9 @@ void main() {
     new Injector().injectDependencies(inetFactory, cacheFactory, firebase: false);
   });
   testWidgets('HomeScaffold test', (WidgetTester tester) async {
-    when(cacheFactory.cacheCourseProvider.getFavorizedCourses()).thenAnswer((_) => Future.value(<Course>[]));
+    Department department = new Department(1, 1, "a", 0);
+    User user = new User("benutzername", "vorname", "nachname", department, "token", 42);
+    when(cacheFactory.cacheCourseProvider.getFavorizedCourses(user)).thenAnswer((_) => Future.value(<Course>[]));
     Content content = new Content();
     await tester.pumpWidget(new MaterialApp(home: new HomeScaffold(content)));
   });
